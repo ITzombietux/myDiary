5cdbe649adda9b304660ba868e7291fabb052545
Mac <mac@macui-macbook-pro-2.local>
2018-10-23 12:51:31 +0900

    Initial Commit

---
+++ myDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myDiary
+//
+//  Created by Mac on 2018. 10. 23..
+//  Copyright © 2018년 zombietux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
